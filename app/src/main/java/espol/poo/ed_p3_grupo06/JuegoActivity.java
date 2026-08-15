adbddd0dd5173ff4f8c141b9cea520f5f8a0ceb8
Frnk29 <frcampoz@espol.edu.ec>
2026-08-15 13:15:21 -0500

    Se modifico el metodo onCreate del MainActivity, ademas de agregar en el metodo JuegoActivity los atributos necesarios e importes para la posterior crearcion de sus metodos, por ahora se agregaron 4 metodos a esta clase. Y como ultimo se agrego una linea de codigo en el AndroidManifest con el objetivo de reflejar la pantalla del juego

--- app/src/main/java/espol/poo/ed_p3_grupo06/JuegoActivity.java
+++ app/src/main/java/espol/poo/ed_p3_grupo06/JuegoActivity.java
@@ -1,4 +1,66 @@
 package espol.poo.ed_p3_grupo06;
 
-public class JuegoActivity {
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.ArrayList;
+import java.util.List;
+import espol.poo.ed_p3_grupo06.modelo.Tablero;
+import espol.poo.ed_p3_grupo06.modelo.MiniMax;
+import espol.poo.ed_p3_grupo06.modelo.ArbolNario;
+import espol.poo.ed_p3_grupo06.modelo.Nodo;
+
+public class JuegoActivity extends AppCompatActivity{
+    private Tablero tablero;
+    private MiniMax minimax;
+    private char turnoHumano;
+    private char turnoPC;
+    private char turnoActual;
+    private boolean juegoTerminado = false;
+
+    private TextView tvEstado;
+    private List<Button> botonesTablero;
+    private Button btnVolver;
+    private SharedPreferences prefs;
+
+    private void limpiarPartidaLocal() {
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.clear();
+        editor.apply();
+    }
+
+    private void verificarTurno() {
+        if (tablero.hayGanador(turnoHumano)) {
+            tvEstado.setText("¡Felicidades, ganaste!");
+            finalizarPartida();
+        } else if (tablero.hayGanador(turnoPC)) {
+            tvEstado.setText("La Computadora Gana.");
+            finalizarPartida();
+        } else if (tablero.estaLleno()) {
+            tvEstado.setText("¡Es un Empate!");
+            finalizarPartida();
+        } else {
+            if (turnoActual == turnoHumano) {
+                tvEstado.setText("Es tu turno (" + turnoHumano + ")");
+            } else {
+                // El delay en este caso es para dar un pequeño instante de tiempo y que la pc no juegue al instante.
+                tvEstado.postDelayed(this::procesarJugadaPC, 300); //Pendiente a la creacion del metodo procesarJugadaPC
+            }
+        }
+    }
+
+    private void finalizarPartida() {
+        juegoTerminado = true;
+        limpiarPartidaLocal(); // Borramos el registro para que la próxima vez partida la comience desde 0
+    }
+
+    private void actualizarInterfaz() {
+        List<Character> celdas = tablero.getCeldas();
+        for (int i = 0; i < 9; i++) {
+            char simbolo = celdas.get(i);
+            botonesTablero.get(i).setText(simbolo == '-' ? "" : String.valueOf(simbolo));
+        }
+    }
 }
--- app/src/main/java/espol/poo/ed_p3_grupo06/MainActivity.java
+++ app/src/main/java/espol/poo/ed_p3_grupo06/MainActivity.java
@@ -1,17 +1,37 @@
 package espol.poo.ed_p3_grupo06;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.Button;
+import android.widget.RadioGroup;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RadioGroup rgSimbolo, rgTurno;
+    private Button btnJugar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+        rgSimbolo = findViewById(R.id.rgSimbolo);
+        rgTurno = findViewById(R.id.rgTurno);
+        btnJugar = findViewById(R.id.btnJugar);
+
+        btnJugar.setOnClickListener(v -> {
+            // 1. Obtener símbolo elegido
+            char simboloHumano = rgSimbolo.getCheckedRadioButtonId() == R.id.rbX ? 'X' : 'O';
+
+            // 2. Obtener quién inicia
+            boolean iniciaHumano = rgTurno.getCheckedRadioButtonId() == R.id.rbYo;
+
+            // 3. Enviar datos a la siguiente pantalla
+            Intent intent = new Intent(MainActivity.this, JuegoActivity.class);
+            intent.putExtra("SIMBOLO_HUMANO", simboloHumano);
+            intent.putExtra("INICIA_HUMANO", iniciaHumano);
+            startActivity(intent);
+        });
     }
 }
